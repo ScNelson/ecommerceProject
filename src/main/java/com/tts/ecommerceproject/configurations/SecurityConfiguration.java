b60511a05c89c7070d871c4b5c695a401658f71a
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-07 00:50:20 -0500

    Updated configurations

--- src/main/java/com/tts/ecommerceproject/configurations/SecurityConfiguration.java
+++ src/main/java/com/tts/ecommerceproject/configurations/SecurityConfiguration.java
@@ -26,8 +26,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/cart").authenticated().and().formLogin().loginPage("/signin")
-                .loginProcessingUrl("/login").and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
-                .logoutSuccessUrl("/");
+        http.authorizeRequests().antMatchers("/cart").authenticated().antMatchers("/charge").authenticated().and()
+                .formLogin().loginPage("/signin").loginProcessingUrl("/login").and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/signout")).logoutSuccessUrl("/");
     }
 }
