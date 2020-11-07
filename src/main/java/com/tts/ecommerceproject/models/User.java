66be25b2925177b43c68756cf90200e07bdebb75
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-07 00:52:55 -0500

    Clean up code

--- src/main/java/com/tts/ecommerceproject/models/User.java
+++ src/main/java/com/tts/ecommerceproject/models/User.java
@@ -3,123 +3,106 @@ package com.tts.ecommerceproject.models;
 import java.util.Collection;
 import java.util.Map;
 
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotEmpty;
 
-import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 public class User implements UserDetails {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = -3789676370267015756L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    @Id // Specifies primary key
-    @GeneratedValue(strategy = GenerationType.AUTO) // Key automatically generated
-    @Column(name = "user_id") // Changes the name in database
-    private Long id;
+	@NotEmpty
+	private String username;
 
-    @Length(min = 3, message = "Your username must have at least 3 characters")
-    @Length(max = 15, message = "Your username cannot have more than 15 characters")
-    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
-    private String username;
+	@NotEmpty
+	private String password;
 
-    @Length(min = 5, message = "Your password must have at least 5 characters")
-    private String password;
+	@ElementCollection
+	private Map<Product, Integer> cart;
 
-    @ElementCollection
-    private Map<Product, Integer> cart; 
+	public User(@NotEmpty String username, @NotEmpty String password, Map<Product, Integer> cart) {
+		this.username = username;
+		this.password = password;
+		this.cart = cart;
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public User() {
+	}
 
-    @Override
-    public String getPassword() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    @Override
-    public String getUsername() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public User() {}
+	public Map<Product, Integer> getCart() {
+		return cart;
+	}
 
-    public User(Long id, String username, String password, Map<Product,Integer> cart) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.cart = cart;
-    }
+	public void setCart(Map<Product, Integer> cart) {
+		this.cart = cart;
+	}
 
-    public Long getId() {
-        return this.id;
-    }
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", cart=" + cart + "]";
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+	// TODO Auto-generated method stub
+	return null;
+	}
 
-    public Map<Product,Integer> getCart() {
-        return this.cart;
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+	// TODO Auto-generated method stub
+	return true;
+	}
 
-    public void setCart(Map<Product,Integer> cart) {
-        this.cart = cart;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+	// TODO Auto-generated method stub
+	return true;
+	}
 
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", username='" + getUsername() + "'" +
-            ", password='" + getPassword() + "'" +
-            ", cart='" + getCart() + "'" +
-            "}";
-    }
+	@Override
+	public boolean isCredentialsNonExpired() {
+	// TODO Auto-generated method stub
+	return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+	// TODO Auto-generated method stub
+	return true;
+	}
 
 }
