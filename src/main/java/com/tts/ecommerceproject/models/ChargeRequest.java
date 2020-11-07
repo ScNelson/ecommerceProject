7ac7ea81395f6bea8d3cf2528d5f26ffe639db65
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-07 00:52:01 -0500

    init from TTS slides

---
+++ src/main/java/com/tts/ecommerceproject/models/ChargeRequest.java
@@ -0,0 +1,68 @@
+package com.tts.ecommerceproject.models;
+
+public class ChargeRequest {
+ 
+    public enum Currency {
+        EUR, USD;
+    }
+    private String description;
+    private int amount;
+    private Currency currency;
+    private String stripeEmail;
+    private String stripeToken;
+
+    public ChargeRequest() {
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return this.currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getStripeEmail() {
+        return this.stripeEmail;
+    }
+
+    public void setStripeEmail(String stripeEmail) {
+        this.stripeEmail = stripeEmail;
+    }
+
+    public String getStripeToken() {
+        return this.stripeToken;
+    }
+
+    public void setStripeToken(String stripeToken) {
+        this.stripeToken = stripeToken;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " description='" + getDescription() + "'" +
+            ", amount='" + getAmount() + "'" +
+            ", currency='" + getCurrency() + "'" +
+            ", stripeEmail='" + getStripeEmail() + "'" +
+            ", stripeToken='" + getStripeToken() + "'" +
+            "}";
+    }
+}
